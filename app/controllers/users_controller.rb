b1c1fb8a49575ce71bc119f2b5f5b1e9d574f12a
pmoorcraft <pedro.moorcraft@gmail.com>
2014-11-18 16:42:26 +0000

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+  										:following, :followers]
   before_action :correct_user, only: [:edit, :update] 
   before_action :admin_user, only: :destroy
+  
   
   def new
     @user = User.new
